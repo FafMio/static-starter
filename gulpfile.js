9c64d769c1cffa4e38a157dec484166b817a7732
Sylvain Roux <fafmio@outlook.fr>
2022-05-26 22:32:55 +0200

    Update gulp
    Added rename module
    fix scss export
    added files type do paths
    try to add js build

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ let gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
+    rename = require("gulp-rename"),
     browserSync = require('browser-sync'),
     del = require('del'),
     fs = require('fs'),
@@ -17,64 +18,84 @@ let gulp = require('gulp'),
 //? Using `path.sep` for kernel compatibilities (Windows, Linux, MacOS)
 let paths = {
     styles: {
-        src: 'src' + path.sep + 'styles' + path.sep + '**' + path.sep + '*.scss',
-        dir: 'src' + path.sep + 'styles' + path.sep + '',
-        dest: 'build' + path.sep + 'assets' + path.sep + 'css' + path.sep + ''
+        src: '.' + path.sep + 'src' + path.sep + 'styles' + path.sep + '**' + path.sep + '*.scss',
+        dir: '.' + path.sep + 'src' + path.sep + 'styles' + path.sep + '',
+        files: '**' + path.sep + '*.scss',
+        dest: '.' + path.sep + 'build' + path.sep + 'assets' + path.sep + 'css' + path.sep + ''
     },
     scripts: {
-        src: 'src' + path.sep + 'scripts' + path.sep + '**' + path.sep + '*.scss',
-        dir: 'src' + path.sep + 'scripts' + path.sep + '',
-        dest: 'build' + path.sep + 'assets' + path.sep + 'js' + path.sep + ''
+        src: '.' + path.sep + 'src' + path.sep + 'scripts' + path.sep + '**' + path.sep + '*.js',
+        dir: '.' + path.sep + 'src' + path.sep + 'scripts' + path.sep + '',
+        files: '**' + path.sep + '*.js',
+        dest: '.' + path.sep + 'build' + path.sep + 'assets' + path.sep + 'js' + path.sep + ''
     },
     build: {
-        dest: 'build' + path.sep + '',
+        dest: '.' + path.sep + 'build' + path.sep + '',
     },
     twig: {
-        src: 'src' + path.sep + 'templates' + path.sep + 'pages' + path.sep + '**' + path.sep + '*.twig',
-        dir: 'src' + path.sep + 'templates' + path.sep + 'pages' + path.sep + '',
-        dest: 'build' + path.sep + ''
+        src: '.' + path.sep + 'src' + path.sep + 'templates' + path.sep + 'pages' + path.sep + '**' + path.sep + '*.twig',
+        dir: '.' + path.sep + 'src' + path.sep + 'templates' + path.sep + 'pages' + path.sep + '',
+        files: '**' + path.sep + '*.twig',
+        dest: '.' + path.sep + 'build' + path.sep + ''
     },
     datas: {
-        dir: 'src' + path.sep + 'datas' + path.sep + '',
+        dir: '.' + path.sep + 'src' + path.sep + 'datas' + path.sep + '',
     }
 };
 
+//* Building the CSS from Sass
 gulp.task('scss', () => {
-    return gulp.src(paths.styles.src)
-        .pipe(
-            sass({ outputStyle: 'compressed' })
-                .on('error', function (err) {
-                    console.log(err.message);
-                    this.emit('end');
-                })
-        )
+    return gulp.src(paths.styles.dir + paths.styles.files)
+        //? If there is errors
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', function (err) { console.log(err.message) }))
+        //? Set destination folder.
+        .pipe(rename((file) => {
+            file.dirname = '';
+            file.basename = "style";
+        }))
         .pipe(gulp.dest(paths.styles.dest));
 });
 
-//*Building Twig to HTML Files
+//* Building Twig to HTML Files
 gulp.task('twig', () => {
     return gulp.src(paths.twig.src)
-        .pipe(plumber({
-            errorHandler: (err) => {
-                console.log(err);
-            }
-        }))
+        .pipe(plumber({ errorHandler: (err) => { console.log(err) } }))
         .pipe(data((file) => {
             let fileDir = path.dirname(file.path).replace(__filename.replace('gulpfile.js', ''), '') + path.sep + path.basename(file.path);
             let fileData = fileDir.replace(paths.twig.dir, paths.datas.dir).replace('.twig', '.json');
 
+            //? Render Twig template with data if data.json exist.
+            //? Else, juste render the Twig template.
             fileExists(fileData)
                 .then((exist) => {
-                    if(exist) {return JSON.parse(fs.readFileSync(fileData))};
+                    if (exist) { return JSON.parse(fs.readFileSync(fileData)) };
                 });
         }))
         .pipe(twig().on('error', (err) => {
             process.stderr.write(err.message + '\n');
         }))
-        .pipe(gulp.dest(paths.build.dest));
-})
+        //? Set destination folder.
+        .pipe(gulp.dest(paths.twig.dest));
+});
 
-
+//* Delete the 'build' file.
 gulp.task('clean', () => {
     return del([paths.build.dest]);
-})
+});
+
+//* Building Javascripts
+//! Not working yet
+//TODO Have to continue the scripts renderer
+gulp.task('js', () => {
+    return gulp.src(paths.scripts.src)
+    .pipe(concat(paths.scripts.src))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.scripts.dest));
+});
+
+//* Creating the watchers
+gulp.task('watch', () => {
+    gulp.watch(paths.styles.dir, ['scss']);
+    // gulp.watch(paths.scripts.dir, ['js', browserSync.reload()]);
+    // gulp.watch(paths.twig.dir, ['twig', browserSync.reload()]);
+});
