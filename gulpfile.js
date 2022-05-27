768b94fe5f442a96010e26f9e1a8a281bf845909
Sylvain Roux <fafmio@outlook.fr>
2022-05-27 10:55:06 +0200

    Fix JS Files to build

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,10 @@ let gulp = require('gulp'),
     path = require('path'),
     data = require('gulp-data'),
     twig = require('gulp-twig'),
-    prefix = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
     sass = require('gulp-sass')(require('sass')),
     plumber = require('gulp-plumber'),
-    concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
     rename = require("gulp-rename"),
-    merge = require("gulp-merge"),
     browserSync = require('browser-sync').create(),
     del = require('del'),
     fs = require('fs'),
@@ -43,7 +40,7 @@ let paths = {
         src: 'src' + path.sep + 'images' + path.sep + '**' + path.sep + '*.{gif,jpg,png,svg}',
         dir: 'src' + path.sep + 'images' + path.sep + '',
         files: '**' + path.sep + '*.twig',
-        dest: 'build' + path.sep + ''
+        dest: 'build' + path.sep + 'assets' + path.sep + 'images' + path.sep + ''
     },
     datas: {
         dir: 'src' + path.sep + 'datas' + path.sep + '',
@@ -69,6 +66,7 @@ gulp.task('sass', () => {
 gulp.task('twig', () => {
     return gulp.src(paths.twig.src)
         .pipe(plumber({ errorHandler: (err) => { console.log(err) } }))
+        //? Compile twig files with their own data with own json file if there is one.
         .pipe(data((file) => {
             let fileDir = path.dirname(file.path).replace(__filename.replace('gulpfile.js', ''), '') + path.sep + path.basename(file.path);
             let fileData = fileDir.replace(paths.twig.dir, paths.datas.dir).replace('.twig', '.json');
@@ -81,6 +79,7 @@ gulp.task('twig', () => {
                     if (exist) {return JSON.parse(fs.readFileSync(fileData)) };
                 });
         }))
+        //? check if there is error in twig files.
         .pipe(twig().on('error', (err) => {
             process.stderr.write(err.message + '\n');
         }))
@@ -95,16 +94,21 @@ gulp.task('twig', () => {
 
 //* Delete the 'build' file.
 gulp.task('clean', () => {
+    //? Delete the build folder.
     return del([paths.build.dest]);
 });
 
 //* Building Javascripts
-//! Not working yet
-//TODO Have to continue the scripts renderer
-gulp.task('js', () => {
+gulp.task('js',() => {
     return gulp.src(paths.scripts.src)
-    .pipe(concat(paths.scripts.src))
-    .pipe(sourcemaps.write('.'))
+    //? Babel compiler
+    .pipe(babel())
+    //? Rename the dirname to set the destination.
+    .pipe(rename((file) => {
+        file.dirname += path.sep;
+        file.dirname = (file.dirname).replace(paths.scripts.dir, '').replace('build' + path.sep, '');
+    }))
+    //? Set destination folder.
     .pipe(gulp.dest(paths.scripts.dest));
 });
 
@@ -115,6 +119,7 @@ gulp.task('serve', gulp.series('clean', 'sass', 'twig', () => {
     gulp.watch(paths.twig.src.split(path.sep).join('/'), gulp.series('twig')).on('change', () => {browserSync.reload();});
 }))
 
+//* Make build
 gulp.task('build', gulp.series('clean', 'sass', 'twig'));
 
 gulp.task('test', () => {
@@ -122,4 +127,5 @@ gulp.task('test', () => {
     console.log();
 })
 
+//* Default command : start the serve
 gulp.task('default', gulp.series('serve'))
